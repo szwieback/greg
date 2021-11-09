0490c4431bd8f333f8b6a004a83fb09a1ca2e9b7
Simon Zwieback <szwieback@alaska.edu>
2021-11-08 16:29:46 -0900

    __init__

--- greg/__init__.py
+++ greg/__init__.py
@@ -3,7 +3,7 @@ Created on Oct 27, 2021
 
 @author: simon
 '''
-from preproc import correlation, force_doubly_nonnegative, covariance_matrix, valid_G
-from simulation import circular_normal, decay_model
-from linking import EMI
-from hadamard import hadreg
+from greg.preproc import correlation, force_doubly_nonnegative, covariance_matrix, valid_G
+from greg.simulation import circular_normal, decay_model
+from greg.linking import EMI
+from greg.hadamard import hadreg
