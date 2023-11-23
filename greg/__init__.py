6baccc2a349790e9bd2a43a2a9d21cd9a283877f
Simon Zwieback <szwieback@alaska.edu>
2023-11-22 15:18:33 -0900

    __init__

--- greg/__init__.py
+++ greg/__init__.py
@@ -7,7 +7,7 @@ from greg.preproc import (
     correlation, force_doubly_nonnegative, covariance_matrix, valid_G, regularize_G,
     paramorder, vectorize_tril, assemble_tril, diagonal)
 from greg.simulation import circular_normal, decay_model
-from greg.linking import EMI
+from greg.linking import EMI, EVD, EVD_py, EMI_py
 from greg.hadamard import hadreg, hadcreg, hadspecreg
 from greg.ioput import  enforce_directory, load_object, save_object, read_parameters
 from greg.accuracy import circular_accuracy, bias
